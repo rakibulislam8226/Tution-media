4b0b41e2f3abc98c579db8c87deefd8dbf12c865
rakibulislam8226 <rakibulislam8226@gmail.com>
2023-11-18 18:50:16 +0600

    project structure updated

---
+++ src/userprofile/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import UserProfile
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name")
+
+
+class UserProfileForm(forms.ModelForm):
+    date_of_birth = forms.DateField(widget=forms.TextInput(attrs={"type": "date"}))
+
+    class Meta:
+        model = UserProfile
+        exclude = ("user",)
